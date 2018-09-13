9b2e2ef4c41ce69aca6263ae3d1ec41f1279bacc
Richard Creeger <richard.creeger@gmail.com>
2018-09-13 15:19:41 -0700

    exercise complete

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+app.post('/create/:name', function (req,res) {
+  res.json({
+    "id": 1,
+    "name": req.params.name
+  })
+})
+
+app.get('/', function (req,res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/verify/:age', function(req,res) {
+  let age = parseInt(req.params.age)
+  if (age > 13) {
+    res.send('200')
+  } else {
+    res.send('403')
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+let fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.post('/create/:name/:age', function(req, res) {
+  let storage = fs.readFileSync(__dirname + '/storage.json', 'utf-8');
+  let newData = JSON.parse(storage);
+  let nameAge = {
+    name: req.params.name,
+    age: req.params.age
+  };
+  newData.push(nameAge);
+  fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(newData));
+  res.send(newData);
+});
+
+app.get('/', function(req, res) {
+  let storage = fs.readFileSync(__dirname + '/storage.json', 'utf-8');
+    res.send(JSON.parse(storage));
+});
+
+app.get('/:name', function(req, res) {
+    let storage = fs.readFileSync(__dirname + '/storage.json', 'utf-8');
+    let parsedData = JSON.parse(storage);
+    let user = parsedData.filter((item) => {
+      return item.name === req.params.name;
+    });
+    if (user.length >= 1) {
+      res.json(user[0]);
+    } else {
+      res.sendStatus(400);
+    }
 });
 
 app.use(function(req, res) {
